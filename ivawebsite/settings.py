0d895c2e79b21ce318ab70f053a5747fd0265d25
IkechukwuKlinsman <ikechukwuklinsman@gmail.com>
2022-11-26 16:06:07 -1100

    new

--- ivawebsite/settings.py
+++ ivawebsite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-uj&l1^@o+suhfligsiruu57oe5c^c_h8zcqh8c@pw=jn_5f$%o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
